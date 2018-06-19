41c9e90ae2b595de9886c8779ca363744fca504e
XLogus <XLogus@users.noreply.github.com>
2018-06-19 15:55:09 -0500

    Cambio version Android

--- js/script.js
+++ js/script.js
@@ -166,3 +166,16 @@ function hashParams(hash) {
 	    
 	    return ret
 };
+
+
+// Detectar ID y modelo de celular
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    var element = document.getElementById('deviceProperties');
+    element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                        'Device Cordova: '  + device.cordova  + '<br />' +
+                        'Device Platform: ' + device.platform + '<br />' +
+                        'Device UUID: '     + device.uuid     + '<br />' +
+                        'Device Version: '  + device.version  + '<br />';
+}
